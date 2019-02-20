17b204c68208e70bd8f27f8200a77e1e382de5f6
Luis Alva <lalva@csumb.edu>
2019-02-20 21:39:05 +0000

    files

--- Projects/project2/functions.js
+++ Projects/project2/functions.js
@@ -1,6 +1,9 @@
 $( "#sub1" ).click(function() {
     
-    $("#content").hide(); 
+    $("#content").hide();
+    $("#sub2").show(); 
+    $("#resultPage").show();
+    // $("#content").empty(); 
     
     var firstName = $("#firstName").val(); 
     var lastName = $("#lastName").val();  
@@ -29,15 +32,19 @@ $( "#sub1" ).click(function() {
     
     
     // $("#resultPage").html("<h1> I feel like I already know you " + firstName + " " + lastName + "</h1>");
+    if(firstName == "" || lastName == ""){
+        $("#resultPage").html("<h1>You didn't tell me your name!</h1>");
+    }else{
+        
     $("#resultPage").html("<h1>Nice to meet you " + firstName + " " + lastName + "!</h1>");
-    
+    }
     if(selectedLabelHtml == "High School Diploma"){
         $("#resultPage").append("<h1>I also only have a High School Diploma so far!</h1>");
     }else if (selectedLabelHtml == "Bachelors"){
         $("#resultPage").append("<h1>I see you have a Bachelors, I  am working towards my Bachelors!</h1>");
     }else if(selectedLabelHtml == "Graduate School"){
         $("#resultPage").append("<h1>Wow Graduate School that must have been hard! I'm impressed!</h1>");
-    }else{
+    }else if(selectedLabelHtml == "Other"){
         $("#resultPage").append("<h1>I see you chose other, now I wonder what you have been up to! </h1>");
     }
     
@@ -47,7 +54,7 @@ $( "#sub1" ).click(function() {
         $("#resultPage").append("<h1>Wow you walk everywhere! You must be healthy!</h1>");
     }else if(transportation == "Bike"){
         $("#resultPage").append("<h1>Using a bike must get tiring but I'm sure it is good for you and the environment</h1>");
-    }else{
+    }else if( transportation == "Another"){
         $("#resultPage").append("<h1>I know you don't use a car so it must mean you get around in a eco friendly way! </h1>");
     }
     
@@ -57,12 +64,17 @@ $( "#sub1" ).click(function() {
         $("#resultPage").append("<h1>I hope whatever made you angry gets resolved soon! </h1>");
     }else if(emotion == "happy"){
         $("#resultPage").append("<h1>Glass half full? Always nice to be in a good mood! </h1>");
-    }else{
+    }else if(emotion == "sad"){
         $("#resultPage").append("<h1>I am sorry you are sad. It does get better.  </h1>");
+    }else if(emotion == "..."){
+        $("#resultPage").append("");
     }
-    
+    if(hobby == ""){
+         $("#resultPage").append("<h1>You didn't tell me a hobby!</h1>"); 
+    }else{
+        
     $("#resultPage").append("<h1>I see your hobby is " + hobby +  ", mine is playing video games.</h1>");
-    
+    }
     
     if(selectedLabelHtml1 == "Mexican"){
         $("#resultPage").append("<h1>I see you like Mexican, me too. Tacos are my favorite!</h1>");
@@ -76,7 +88,17 @@ $( "#sub1" ).click(function() {
         $("#resultPage").append("<h1>I never understood what American food entails but since I live in America I enjoy the food here!</h1>");
     }else if(selectedLabelHtml1=="Korean") {
         $("#resultPage").append("<h1>The only Korean food I'd had is Korean BBQ and that is amazingly good!</h1>");
-    }else{
+    }else if(selectedLabelHtml1 == "I love all food!"){
         $("#resultPage").append("<h1>I love all kinds of food too!</h1>");
     }
+    
+    $("#resultPage").append("<div id='sub3'><input id='sub2' type='button' value='Go Again!' /></div>")
+    
+    
+    $("#sub2").click(function(){
+        $("#content").show(); 
+        $("#sub2").hide(); 
+        $("#resultPage").hide(); 
+    }); 
+    
 });
